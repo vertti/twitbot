4bc7edd715fda76d7e0f75413d836205e07644b7
Janne Sinivirta <janne.sinivirta@gmail.com>
2019-02-15 14:42:16 +0200

    implement tweeting

--- resources/tweet_lambda.ts
+++ resources/tweet_lambda.ts
@@ -1,5 +1,22 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  console.log('Hello World!');
-  const response = JSON.stringify(event, null, 2);
-  return response;
+import Twitter = require('twitter');
+
+export const handler = async ({}: any = {}): Promise<any> => {
+  var client = new Twitter(<Twitter.AccessTokenOptions>{
+    consumer_key: process.env.CONSUMER_API_KEY,
+    consumer_secret: process.env.CONSUMER_API_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_SECRET,
+  });
+  console.log(`Tweeting!`);
+  
+  return new Promise((resolve, reject) => {
+    client.post('statuses/update', { status: 'I am a tweet' }, (error, tweet, {}) => {
+      if (!error) {
+        console.log(tweet);
+        resolve();
+      } else {
+        reject(error);
+      }
+    });
+  });
 }
