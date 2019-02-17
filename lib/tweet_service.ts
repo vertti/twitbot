a41e6b7efd744219046be5e989894e39a4fe0934
Janne Sinivirta <janne.sinivirta@gmail.com>
2019-02-17 15:09:47 +0200

    introduce sqs queue for tweets

--- lib/tweet_service.ts
+++ lib/tweet_service.ts
@@ -1,6 +1,8 @@
 import cdk = require('@aws-cdk/cdk');
 import lambda = require('@aws-cdk/aws-lambda');
 import secretsmanager = require('@aws-cdk/aws-secretsmanager');
+import sqs = require('@aws-cdk/aws-sqs');
+import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
 
 export class TweetService extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string) {
@@ -10,7 +12,9 @@ export class TweetService extends cdk.Construct {
       secretId: 'arn:aws:secretsmanager:eu-west-1:293246570391:secret:v3rtti/twitbot-LKFMxX',
     });
 
-    new lambda.Function(this, 'TweetHandler', {
+    const readyTweetsQueue = new sqs.Queue(scope, 'readyTweetsQueue');
+
+    const fn = new lambda.Function(scope, 'TweetHandler', {
       runtime: lambda.Runtime.NodeJS810,
       code: lambda.Code.directory('resources'),
       handler: 'tweet_lambda.handler',
@@ -19,7 +23,8 @@ export class TweetService extends cdk.Construct {
         CONSUMER_API_SECRET: twitterTokens.jsonFieldValue('CONSUMER_API_SECRET'),
         ACCESS_TOKEN: twitterTokens.jsonFieldValue('ACCESS_TOKEN'),
         ACCESS_SECRET: twitterTokens.jsonFieldValue('ACCESS_SECRET'),
-      }
+      },
     });
+    fn.addEventSource(new SqsEventSource(readyTweetsQueue));
   }
 }
