a35d655348e006c3b68674fe0f0e5e1ab405b184
Janne Sinivirta <janne.sinivirta@gmail.com>
2019-02-17 15:10:17 +0200

    parse tweet from event

--- resources/tweet_lambda.ts
+++ resources/tweet_lambda.ts
@@ -1,16 +1,15 @@
 import Twitter = require('twitter');
 
-export const handler = async ({}: any = {}): Promise<any> => {
-  var client = new Twitter(<Twitter.AccessTokenOptions>{
-    consumer_key: process.env.CONSUMER_API_KEY,
-    consumer_secret: process.env.CONSUMER_API_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_SECRET,
-  });
-  console.log(`Tweeting!`);
-  
+const parseTweetsFromEvent = (event: any): string[] => {
+  if (event.Records && event.Records.length > 0) {
+    return event.Records.map((item: any) => item.body);
+  }
+  return [];
+}
+
+const tweet = (status: string, client: Twitter) => {
   return new Promise((resolve, reject) => {
-    client.post('statuses/update', { status: 'I am a tweet' }, (error, tweet, {}) => {
+    client.post('statuses/update', { status }, (error, tweet, {}) => {
       if (!error) {
         console.log(tweet);
         resolve();
@@ -19,4 +18,19 @@ export const handler = async ({}: any = {}): Promise<any> => {
       }
     });
   });
+}
+
+export const handler = async (event: any = {}): Promise<any> => {
+  var client = new Twitter(<Twitter.AccessTokenOptions>{
+    consumer_key: process.env.CONSUMER_API_KEY,
+    consumer_secret: process.env.CONSUMER_API_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_SECRET,
+  });
+  console.log(`Tweeting! ${JSON.stringify(event)}`);
+  
+  const records = parseTweetsFromEvent(event);
+  if (records.length > 0) {
+    return tweet(records[0], client);
+  }
 }
